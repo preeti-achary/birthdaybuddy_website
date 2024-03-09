2c0a3b3cbb53fb4a4538b90686c6c3c843dc996f
preeti-achary <preetiachary9@gamil.com>
2024-03-09 16:46:07 +0530

    birthdaybuddywebsite

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+     id: 1,
+     name: 'preeti achary',
+     age: 29,
+     image: 'https://www.shutterstock.com/shutterstock/photos/1554086789/display_1500/stock-photo-close-up-portrait-of-yong-woman-casual-portrait-in-positive-view-big-smile-beautiful-model-posing-1554086789.jpg',  
+    },
+    {
+        id: 1,
+        name: 'preeti achary',
+        age: 29,
+        image: 'https://as2.ftcdn.net/v2/jpg/04/38/25/87/1000_F_438258727_5CgCnMCh4w3MzMHg3cTYg2Y1nfgzrktm.jpg',
+    },
+    {
+     id: 1,
+     name: 'preeti achary',
+     age: 29,
+     image: 'https://img.freepik.com/premium-photo/close-up-young-handsome-man-with-beard-smiling-camera-with-confidence-standing-white-background_1258-49635.jpg',
+    },
+    {
+     id: 1,
+     name: 'preeti achary',
+     age: 29,
+     image: 'https://www.un.org/sites/un2.un.org/files/007-alexander-khimushin.jpg',
+    },
+    {
+     id: 1,
+     name: 'preeti achary',
+     age: 29,
+     image: 'https://t3.ftcdn.net/jpg/03/28/19/46/360_F_328194664_RKSHvMLgHphnD1nwQYb4QKcNeEApJmqa.jpg',
+    },
+];
